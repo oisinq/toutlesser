fb83feb0a9c8e876102a3626a1e11c9d51cf11ac
Oisin Quinn <oquinn@hubspot.com>
2019-08-06 11:24:57 +0100

    It works, but it's ugly

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 import feedparser
 import requests
+import time
+import pync
+import random
 
-result = requests.get('https://www.toutless.com/app.php/feed').text
+idSet = set()
+count = 1
 
-d = feedparser.parse(result)
+while True:
+  print(f"Request No.{count}...")
+  result = requests.get('https://www.toutless.com/app.php/feed').text
+  d = feedparser.parse(result)
 
-for entry in d.entries:
-  print(entry.id)
+  for entry in d.entries:
+    if 'Electric Picnic' in entry.title and ':forsale:' in entry.content[0].value and not entry.id in idSet:
+      print(entry.content)
+      idSet.add(entry.id)
+      print("New one: ")
+      print(entry.id)
+      pync.notify('EP ticket found!', open=entry.id)
+    else:
+      print("nope...")
+  
+  count += 1
+  time.sleep(random.uniform(55, 65))
